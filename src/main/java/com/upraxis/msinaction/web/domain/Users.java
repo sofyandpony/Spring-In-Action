fdbcab42daa00d6c71d572df73a92fc7f42df260
Jesse Ballera <jesse_ballera@upraxis.com>
2017-12-27 15:50:24 +0800

    add domain package/class

---
+++ src/main/java/com/upraxis/msinaction/web/domain/Users.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.upraxis.msinaction.web.domain;
+
+import java.io.Serializable;
+import java.util.UUID;
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
+
+/**
+ *
+ * @author jesse
+ */
+@Table("users")
+public class Users implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @PrimaryKey
+    private UUID id;
+    
+    private String firstName;
+    
+    private String middleName;
+    
+    private String lastName;
+
+    public Users() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
